145a4143b24a7155dc87e59c2651ca710b081aed
Arman <armandeeps@gmail.com>
2017-09-10 15:56:26 -0700

    moved seceret

--- app.py
+++ app.py
@@ -38,6 +38,6 @@ def feed():
 def content():
     return clip
 
+app.secret_key = os.urandom(12)
 if __name__ == "__main__":
-    app.secret_key = os.urandom(12)
     app.run(debug=True,host='0.0.0.0', port=4000)
